4e45c5098d3638dbd55ec84d93c66c6cb63f1c3d
MarijaGabarasvili <marija.gabarasvili@gmail.com>
2025-10-07 11:38:37 +0200

    little changes

--- emb_ai/can_detect.py
+++ emb_ai/can_detect.py
@@ -18,37 +18,43 @@ mR  = ev3.LargeMotor('outD')      # right motor
 mG = ev3.MediumMotor('outC')     # gripper motor
 
 ultrasonic = ev3.UltrasonicSensor('in3') # ultrasonic sensor
+MODE_US_DIST_CM = 'US-DIST-CM' #measures distance in cm
+ultrasonic.mode = MODE_US_DIST_CM
 sound = Sound()
+
+#So it already starts searching for a can with an open gripper
+mG.stop_action = "coast" # gripper motor
+mG.run_to_abs_pos(position_sp=1500, speed_sp=300) # open gripper
+time.sleep(1)
+
 
 
 # Detecting can
-def can_detected():
+def can_detect():
     sound.speak("Searching for can")
-    s_time = time.time()
-    spin_duration = 2 # how much seconds to spin before moving forward
-    while ultrasonic.value()>100:
-        el_time = time.time() - s_time
-        # if el_time < spin_duration:
-        #     mL.run_forever(speed_sp=100)
-        #     mR.run_forever(speed_sp=-100) #spins
-        # else:
-        mL.run_forever(speed_sp=100)
-        mR.run_forever(speed_sp=100) #moves forward
+    #s_time = time.time()
+    #spin_duration = 2 # how much seconds to spin before moving forward
+    while ultrasonic.distance_centimeters > 10:
+        # Firstly turn on 45 degrees one side to look for a can
+        mL.run_to_rel_pos(position_sp=100, speed_sp=200)
+        mR.run_to_rel_pos(position_sp=-100, speed_sp=200)
         time.sleep(1) #moves forward for 0.5 seconds
-        s_time = time.time() #reset timer
+        # Turns theother side for 45 degrees to detect a can
+        mL.run_to_rel_pos(position_sp=-200, speed_sp=200)
+        mR.run_to_rel_pos(position_sp=100, speed_sp=200)
+        time.sleep(1) #moves forward for 0.5 seconds
+        
     mL.stop(stop_action="brake")
     mR.stop(stop_action="brake")
     sound.speak("Can detected")
 
 def grab_can():
     sound.speak("Grabbing can")
-    mG.run_to_abs_pos(position_sp=500, speed_sp=300) # open gripper
-    time.sleep(1)
-    mG.run_to_abs_pos(position_sp=-200, speed_sp=300) # close gripper
+    mG.run_to_abs_pos(position_sp=-1900, speed_sp=300) # close gripper
     time.sleep(1)
     sound.speak("Can grabbed")
     
     
-can_detected()
+can_detect()
 grab_can()
     
--- emb_ai/test.py
+++ emb_ai/test.py
@@ -2,14 +2,23 @@
 
 import ev3dev.ev3 as ev3
 import time
+from ev3dev2.sound import Sound
 
 
 mG = ev3.MediumMotor('outC')
+ultrasonic = ev3.UltrasonicSensor('in3') # ultrasonic sensor
+sound = Sound()
+
+
+while ultrasonic.distance_centimeters > 20:
+    sound.speak("Can is not detected")
+    
+sound.speak("Can is detected")
 
 #maybe a setup for robot grabber
-mG.stop_action = "coast" # gripper motor
-mG.run_to_abs_pos(position_sp=1500, speed_sp=300) # open gripper
-time.sleep(2)
-mG.run_to_abs_pos(position_sp=-1900, speed_sp=300) # close gripper
-time.sleep(2)
+# mG.stop_action = "coast" # gripper motor
+# mG.run_to_abs_pos(position_sp=1500, speed_sp=300) # open gripper
+# time.sleep(2)
+# mG.run_to_abs_pos(position_sp=-2000, speed_sp=300) # close gripper
+# time.sleep(2)
 
