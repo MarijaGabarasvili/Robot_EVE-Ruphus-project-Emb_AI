ab36ea285b5f84718029698a85c88c0c30a5122d
MarijaGabarasvili <marija.gabarasvili@gmail.com>
2025-10-20 11:53:10 +0200

    fixed final line follower

--- emb_ai/final_line_follower.py
+++ emb_ai/final_line_follower.py
@@ -12,14 +12,18 @@ csL = ev3.ColorSensor('in1')      # left sensor
 csR.mode = 'COL-REFLECT'
 csL.mode = 'COL-REFLECT'
 
-mL.reset(); mR.reset()
+mL.stop(stop_action="brake")
+mR.stop(stop_action="brake")
+
+print("Left:", csL.value(), "Right:", csR.value())
+
 
 # -------- Tuning --------
 BASE_SPEED   = 70           # forward speed
 MAX_SPEED    = 150           # clamp
 BLACK, WHITE = 10, 60         # quick manual calibration
 THRESHOLD    = (BLACK + WHITE) / 2.0
-DETECT_MARG  = 8             # how close to threshold counts as "on edge"
+DETECT_MARG  = 15            # how close to threshold counts as "on edge"
 SEARCH_TURN  = 120           # search spin speed
 
 # PID gains (start here)
@@ -38,7 +42,7 @@ prev_t   = time.time()
 print("Threshold:", THRESHOLD)
 
 #searching for line function to implement
-
+ 
     
 
 
@@ -52,28 +56,27 @@ try:
         vR = csR.value()
 
         # Decide which sensor to follow
-        left_on  = (vL < THRESHOLD - DETECT_MARG)
-        right_on = (vR < THRESHOLD - DETECT_MARG)
+        left_on  = (vL < THRESHOLD + DETECT_MARG)
+        right_on = (vR < THRESHOLD + DETECT_MARG)
 
-        if left_on and not right_on:
-            if mode != "left":   # reset PID when switching sides
-                integral = 0.0; prev_err = 0.0
-            mode = "left"
-        elif right_on and not left_on:
-            if mode != "right":
-                integral = 0.0; prev_err = 0.0
+        if vL<vR:
             mode = "right"
+        else:
+            mode = "left"
         # if both or none -> keep previous mode
 
 
 
         # Compute PID using the active sensor
-        meas = vR if mode == "right" else vL
-        err  = THRESHOLD - meas
+        if mode == "right":
+            err  = THRESHOLD - vR
+        else:
+            err  = vL - THRESHOLD
+        
         integral += err * dt
         deriv = (err - prev_err) / dt
 
-        u = Kp*err + Ki*integral + Kd*deriv
+        u = -(Kp*err + Ki*integral + Kd*deriv)
 
         # Same motor mix works for both sides with this error definition:
         # positive u -> turn right (left faster)
@@ -94,6 +97,8 @@ try:
         prev_err = err
         prev_t   = t
         time.sleep(0.01)
+        
+
 
 finally:
     mL.stop(stop_action="brake")
