52e9ce3049b045934d45e5794fc101f8452078a6
MarijaGabarasvili <marija.gabarasvili@gmail.com>
2025-10-21 14:48:47 +0200

    final changesS

--- emb_ai/find_line.py
+++ emb_ai/find_line.py
@@ -29,8 +29,8 @@ while True:
     left_on  = (vL < THRESHOLD + DETECT_MARG)
     right_on = (vR < THRESHOLD + DETECT_MARG)
         
-    mL.run_to_rel_pos(position_sp=20, speed_sp=BASE_SPEED)
-    mR.run_to_rel_pos(position_sp=-20, speed_sp=BASE_SPEED)
+    mL.run_to_rel_pos(position_sp=60, speed_sp=BASE_SPEED)
+    mR.run_to_rel_pos(position_sp=-60, speed_sp=BASE_SPEED)
     mL.wait_while('running')
     mR.wait_while('running')
     time.sleep(1.5)
@@ -39,8 +39,8 @@ while True:
         break
 
         # Step 2: Turn a little to the right
-    mL.run_to_rel_pos(position_sp=-40, speed_sp=BASE_SPEED)
-    mR.run_to_rel_pos(position_sp=40, speed_sp=BASE_SPEED)
+    mL.run_to_rel_pos(position_sp=-120, speed_sp=BASE_SPEED)
+    mR.run_to_rel_pos(position_sp=120, speed_sp=BASE_SPEED)
     mL.wait_while('running')
     mR.wait_while('running')
     time.sleep(1.5)
@@ -49,8 +49,8 @@ while True:
         break
 
     # Step 3: Drive forward a little further
-    mL.run_to_rel_pos(position_sp=20, speed_sp=BASE_SPEED)
-    mR.run_to_rel_pos(position_sp=-20, speed_sp=BASE_SPEED)
+    mL.run_to_rel_pos(position_sp=60, speed_sp=BASE_SPEED)
+    mR.run_to_rel_pos(position_sp=-60, speed_sp=BASE_SPEED)
     mL.wait_while('running')
     mR.wait_while('running')
     time.sleep(1.5)
@@ -58,8 +58,8 @@ while True:
     if left_on or right_on:
         break
         
-    mL.run_forever(speed_sp=BASE_SPEED)
-    mR.run_forever(speed_sp=BASE_SPEED)
+    mL.run_forever(speed_sp=-BASE_SPEED)
+    mR.run_forever(speed_sp=-BASE_SPEED)
     time.sleep(1.5)
         
     if left_on or right_on:
