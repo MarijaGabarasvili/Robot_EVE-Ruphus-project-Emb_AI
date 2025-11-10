715c994a6a473eeba97abb6f6c79b1c444563310
VKendy <vera.kenderessy@gmail.com>
2025-11-10 12:53:35 +0100

    Add files via upload
    New file with additional codes

---
+++ emb_ai/example_touch.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3.4
+
+import ev3dev.ev3 as ev3
+import time 
+import sleep
+
+btn = ev3.Button()
+
+mA = ev3.LargeMotor('outA')
+mB = ev3.LargeMotor('outB')
+
+THRESHOLD_LEFT = 30
+THRESHOLD_RIGHT = 350
+
+BASE_SPEED = 30
+TURN_SPEED = 80
+
+TouchSensor = ev3.TouchSensor('in3')
